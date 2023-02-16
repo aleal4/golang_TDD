6efb76498c0ce9f71b8d1de9a05c16eeca95bf58
Augusto Leal <aleal4@gmail.com>
2023-02-15 17:19:45 -0700

    adding area test

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -9,3 +9,11 @@ func TestPerimter(t *testing.T) {
 		t.Errorf("got %.2f want %2.f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+	if got != want {
+		t.Errorf("got %.2f want %2.f", got, want)
+	}
+}
