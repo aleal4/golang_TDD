743c7a652445b6ddcf210d31642b836fbbc0e0fe
Augusto Leal <aleal4@gmail.com>
2023-02-21 22:29:44 -0700

    asseting error

--- maps/directory.go
+++ maps/directory.go
@@ -4,13 +4,14 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("coudl not find the word you were lookng for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
-
 }
 
 func Search(dictionary map[string]string, word string) string {
--- maps/directory_test.go
+++ maps/directory_test.go
@@ -13,20 +13,21 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unkown word", func(t *testing.T) {
-		_, err := dictionary.Search("unkown")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("unkown")
 
-		if err == nil {
-			t.Fatal("expected to get an error")
-		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
 
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
 	}
