d15f8e1534e2f36a2ec639a1f02cb3301fc2d777
Augusto Leal <aleal4@gmail.com>
2023-02-22 22:42:29 -0700

    added multiple tests

--- maps/directory_test.go
+++ maps/directory_test.go
@@ -37,6 +37,15 @@ func TestAdd(t *testing.T) {
 		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, definition)
 	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 
 }
 
