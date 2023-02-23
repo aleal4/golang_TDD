1350970fc63733e0e10cc1b286b78843d69ce4fc
Augusto Leal <aleal4@gmail.com>
2023-02-23 10:28:59 -0700

    implementing a simple delete function on maps

--- maps/directory_test.go
+++ maps/directory_test.go
@@ -90,3 +90,15 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 		t.Errorf("got %q want %q", got, definition)
 	}
 }
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be delted", word)
+	}
+}
